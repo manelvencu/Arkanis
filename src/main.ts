e6445a12dfc37ff291ca1cb166446306ad9ff439
manelvencu <manel@vencu.es>
2026-08-27 14:24:49 +0200

    feat: bootstrap Arkanis game

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import * as Phaser from 'phaser';
+import './style.css';
+import { CharacterSelectScene } from './scenes/CharacterSelectScene';
+import { TrainingScene } from './scenes/TrainingScene';
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  parent: 'game',
+  width: 960,
+  height: 540,
+  backgroundColor: '#111111',
+  pixelArt: true,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false
+    }
+  },
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
+  scene: [CharacterSelectScene, TrainingScene]
+};
+
+new Phaser.Game(config);
